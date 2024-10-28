b96aa11704b8714ca23776ded3934bfdba4b2025
YS <theonlymoby@protonmail.com>
2024-10-28 14:53:33 +0200

    favourites page design

--- lib/pages/favourites.dart
+++ lib/pages/favourites.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:what_eat_flutter/repositories/edamam_api/models/recipe_preview.dart';
+import 'package:what_eat_flutter/widgets/recyclers/recipe_tile.dart';
 
-class FavouritesPage extends StatefulWidget {
-  const FavouritesPage({super.key});
-   @override   State<StatefulWidget> createState() {
-    return FavouritesPageState();   } }
+class FavouritesPage extends StatelessWidget {
 
-class FavouritesPageState extends State<FavouritesPage> {   @override   Widget build(BuildContext context) {
-  return Scaffold(
+  const FavouritesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    late List<dynamic> recipes;
+    late Box favouritesBox; 
+    favouritesBox = Hive.box("favouritesBox");
+    recipes = favouritesBox.values.toList();
+
+    return Scaffold(
       appBar: AppBar(
-        title: const Text("Favorites"),
+        title: const Text("Favourite Recipes"),
+      ),
+      body: recipes.isEmpty
+      ? const Center(
+              child: Text(
+                "No favourites were added yet ;(",
+                style: TextStyle(fontSize: 16.0, color: Colors.grey),
+                textAlign: TextAlign.center,
+              ),
+            )
+      : ListView.separated(
+        itemCount: recipes.length,
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+        itemBuilder: (context, index) {
+          return RecipeTile(recipe: recipes[index]);
+        }, 
       ),
     );
-} 
+  }
 }
 
